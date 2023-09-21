6977bc3fd4a498c8da83aaf0f63524fa225369e4
harshit2636 <142739875+harshit2636@users.noreply.github.com>
2023-09-21 15:00:43 +0530

    Add files via upload

---
+++ palindrome.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class palindrome {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 * @throws NumberFormatException 
+	 */
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		int n,rem=0,rev=0;
+		
+		BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter the no. :");
+		n =Integer.parseInt( br.readLine());
+		int temp = n;
+		while(n!=0){
+			
+		}
+	}
+
+}
